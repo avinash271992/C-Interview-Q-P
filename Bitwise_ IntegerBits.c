d4d2e60fbd0cbe4c385aa1bcbcc9e12c1ed4089a
avinash271992 <avinash.lala@gmail.com>
2017-07-06 14:53:27 +0530

    Three Bit wise Question

--- Bitwise_ IntegerBits.c
+++ Bitwise_ IntegerBits.c
@@ -1,4 +1,4 @@
-1 // C Program to Check if All the Bits of a given Integer is One(1) 
+1  C Program to Check if All the Bits of a given Integer is One(1) 
 #include<stdio.h>
 #include<string.h>
 int main()
@@ -16,4 +16,37 @@ break;
 }
 }
 
-2 
+2 C Program to Count the Number of Trailing Zeroes in Integer 
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int n=16,count=0;
+  while(n !=0)
+  {
+    if(n & 1==1)
+      break;
+    else
+    {
+      count++;
+      n=n>>1;
+    }
+  }
+  printf("count %d\n",count);
+}
+
+3 C Program to find the Highest Bit Set for any given Integer 
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  int n=16,count=0,i=0;
+  for(i=0;i<32;i++)
+  {
+    if(n>>i|0 ==1)
+      count=i;
+  }
+  printf("last bit %d\n",count );
+}
+  
+      
